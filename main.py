2e7eef13fce08588a66560d9745dea8c6793a97d
TopTofu <nafeen.lefa@gmail.com>
2021-03-16 13:53:53 +0100

    hotfix

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import PySimpleGUI as gui
 
 def main():
     config = loadConfig("config.json")
-    
+
     gui.LOOK_AND_FEEL_TABLE['FloraStudyTheme'] = Layout.theme
-    
     gui.theme('FloraStudyTheme')
-    
+
     importedCards = baseWindow.init()
-    
+
     if importedCards is not None:
         mainWindow.init(importedCards, config)
+
 
 if __name__ == '__main__':
     main()
